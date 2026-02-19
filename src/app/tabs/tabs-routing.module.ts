7e0329d46d61233b0549726e06d312e50ae87b2c
Dev Palacio <danielpalaciouribe@gmail.com>
2026-02-19 15:55:01 -0500

    feat: implement todo app with categories, filtering and android cordova build

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tasks',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../home/home.module').then((m) => m.HomePageModule),
+          },
+        ],
+      },
+      {
+        path: 'categories',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../pages/categories/categories.module').then(
+                (m) => m.CategoriesPageModule,
+              ),
+          },
+        ],
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/tasks',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+})
+export class TabsPageRoutingModule {}
